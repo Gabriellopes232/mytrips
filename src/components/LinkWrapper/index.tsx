aacc939253ab699adea3f367765dcc0c79d9ddfb
Gabriellopes232 <xdynatz@gmail.com>
2022-07-25 09:20:24 -0300

    feat: component LinkWrapper

--- src/components/LinkWrapper/index.tsx
+++ src/components/LinkWrapper/index.tsx
@@ -1,5 +1,15 @@
-import React from 'react'
+import Link from 'next/link'
+import * as S from './styles'
 
-export default function LinkWrapper() {
-  return <div>index</div>
+type LinkWrapperProps = {
+  href: string
+  children: React.ReactNode
 }
+
+const LinkWrapper = ({ href, children }: LinkWrapperProps) => (
+  <S.Wrapper>
+    <Link href={href}>{children}</Link>
+  </S.Wrapper>
+)
+
+export default LinkWrapper
