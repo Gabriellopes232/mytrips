89927c4613983a93e4280029f6b7d485077a3a9a
Gabriellopes232 <xdynatz@gmail.com>
2022-07-25 09:39:14 -0300

    feat: create template about page

---
+++ src/templates/About/index.tsx
@@ -0,0 +1,21 @@
+import * as S from './styles'
+import { CloseOutline } from '@styled-icons/evaicons-outline'
+import LinkWrapper from 'components/LinkWrapper'
+
+const AboutTemplate = () => (
+  <S.Content>
+    <LinkWrapper href="/">
+      <CloseOutline size={32}></CloseOutline>
+    </LinkWrapper>
+    <S.Body>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+        voluptatem corrupti est iusto accusantium excepturi deleniti a libero
+        quaerat quae reiciendis dolorum ipsam mollitia error debitis nisi,
+        dolorem quasi reprehenderit.
+      </p>
+    </S.Body>
+  </S.Content>
+)
+
+export default AboutTemplate
