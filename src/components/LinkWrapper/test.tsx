c26b55ad55f8c3fa8cf4e5a711f9b690625422b0
Gabriellopes232 <xdynatz@gmail.com>
2022-07-25 19:59:22 -0300

    refactor: fix test

--- src/components/LinkWrapper/test.tsx
+++ src/components/LinkWrapper/test.tsx
@@ -1,11 +1,11 @@
 import { render, screen } from '@testing-library/react'
-import LinkWrapper from './index'
+import LinkWrapper from '.'
 
-describe('<LinkWrapper/>', () => {
+describe('<LinkWrapper />', () => {
   it('should render link and children', () => {
     render(<LinkWrapper href="/my-link">Anything</LinkWrapper>)
 
-    const children = screen.getAllByRole('link', { name: /anything/i })
+    const children = screen.getByRole('link', { name: /anything/i })
 
     expect(children).toBeInTheDocument()
     expect(children).toHaveAttribute('href', '/my-link')
