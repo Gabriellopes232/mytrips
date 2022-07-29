6c2b7064d439909fca6e0f5e66a0979d1fcd78e3
Gabriellopes232 <xdynatz@gmail.com>
2022-07-28 22:10:49 -0300

    feat: generate static routes

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,16 +1,26 @@
 import client from 'graphql/client'
 import { GET_PAGES } from 'graphql/queries'
-import AboutTemplate from 'templates/About'
+import { useRouter } from 'next/router'
+import PageTemplate from 'templates/Pages'
 
-export default function AboutPage() {
-  return <AboutTemplate />
+type Props = {
+  slug: any
 }
 
-export const getStaticProps = async () => {
-  const { pages } = await client.request(GET_PAGES)
+export default function AboutPage() {
+  const router = useRouter()
 
-  console.log(pages)
-  return {
-    props: {}
-  }
+  if (router.isFallback) return null
+
+  return <PageTemplate />
+}
+
+export async function getStaticPaths() {
+  const { pages } = await client.request(GET_PAGES, { first: 3 })
+
+  const paths = pages.map(({ slug }: Props) => ({
+    params: { slug }
+  }))
+
+  return { paths, fallback: true }
 }
