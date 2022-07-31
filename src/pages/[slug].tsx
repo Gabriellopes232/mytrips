1e47482050c9f1fb0825e7c1b59f2963451f9fec
Gabriellopes232 <xdynatz@gmail.com>
2022-07-30 23:08:50 -0300

    refactor: types routes from graphql

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -3,10 +3,7 @@ import client from 'graphql/client'
 import { GET_PAGES, GET_PAGE_BY_SLUG } from 'graphql/queries'
 import { useRouter } from 'next/dist/client/router'
 import PageTemplate, { PageTemplateProps } from 'templates/Pages'
-
-type Props = {
-  slug: any
-}
+import { GetPageBySlugQuery, GetPagesQuery } from 'generated/graphql'
 
 export default function Page({ heading, body }: PageTemplateProps) {
   const router = useRouter()
@@ -17,9 +14,9 @@ export default function Page({ heading, body }: PageTemplateProps) {
 }
 
 export async function getStaticPaths() {
-  const { pages } = await client.request(GET_PAGES, { first: 3 })
+  const { pages } = await client.request<GetPagesQuery>(GET_PAGES, { first: 3 })
 
-  const paths = pages.map(({ slug }: Props) => ({
+  const paths = pages.map(({ slug }) => ({
     params: { slug }
   }))
 
@@ -27,7 +24,7 @@ export async function getStaticPaths() {
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const { page } = await client.request(GET_PAGE_BY_SLUG, {
+  const { page } = await client.request<GetPageBySlugQuery>(GET_PAGE_BY_SLUG, {
     slug: `${params?.slug}`
   })
 
@@ -36,7 +33,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   return {
     props: {
       heading: page.heading,
-      body: page.body.html
+      body: page.body?.html
     }
   }
 }
