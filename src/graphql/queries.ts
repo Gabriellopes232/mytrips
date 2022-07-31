5eb0c99827a4a02e4468d19d835c3e2a7be3b23a
Gabriellopes232 <xdynatz@gmail.com>
2022-07-31 11:03:25 -0300

    feat: queries places

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -24,3 +24,24 @@ export const GET_PAGE_BY_SLUG = gql`
     }
   }
 `
+export const GET_PLACES = gql`
+  query getPlaces {
+    places {
+      id
+      slug
+      name
+      location {
+        latitude
+        longitude
+      }
+      description {
+        html
+      }
+      gallery {
+        url
+        height
+        width
+      }
+    }
+  }
+`
