bf893ef61f75b4b44796cec1f8019d8d0a72d6fc
Gabriellopes232 <xdynatz@gmail.com>
2022-08-01 20:22:30 -0300

    feat: template page places route

---
+++ src/templates/Places/index.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+import React from 'react'
+
+type ImageProps = {
+  url: string
+  height: number
+  width: number
+}
+
+export type PlacesTemplateProps = {
+  place: {
+    slug: string
+    name: string
+    description: {
+      html: string
+    }
+    gallery: ImageProps[]
+  }
+}
+
+export default function PlacesTemplate({ place }: PlacesTemplateProps) {
+  return (
+    <>
+      <h1>{place.name}</h1>
+
+      <div dangerouslySetInnerHTML={{ __html: place.description.html }} />
+
+      {place.gallery.map((image, index) => (
+        <Image key={`photo-${index}`} src={image.url} alt={place.name} />
+      ))}
+    </>
+  )
+}
