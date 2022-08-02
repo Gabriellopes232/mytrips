30ee03579f8b0628c30909148d8b05c14e7b7e18
Gabriellopes232 <xdynatz@gmail.com>
2022-08-02 09:36:00 -0300

    fix: wrong imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import HomeTemplate from 'templates/Home'
 import { MapProps } from 'components/Map'
 import client from 'graphql/client'
 import { GET_PLACES } from 'graphql/queries'
-import { GetPlacesQuery } from 'generated/graphql'
+import { GetPlacesQuery } from 'graphql/generated/graphql'
 
 export default function Home({ places }: MapProps) {
   return <HomeTemplate places={places} />
