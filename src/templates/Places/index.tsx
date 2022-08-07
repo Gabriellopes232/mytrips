48e3586ca558b94a574769aefab9fdba9b1bc60c
Gabriellopes232 <gabriel.lopes.nascimento.dev@gmail.com>
2022-08-07 12:37:57 -0300

    feat: seo pages

--- src/templates/Places/index.tsx
+++ src/templates/Places/index.tsx
@@ -3,6 +3,7 @@ import LinkWrapper from 'components/LinkWrapper'
 import Image from 'next/image'
 import * as S from './styles'
 import { useRouter } from 'next/dist/client/router'
+import { NextSeo } from 'next-seo'
 
 type ImageProps = {
   url: string
@@ -16,6 +17,7 @@ export type PlacesTemplateProps = {
     name: string
     description?: {
       html: string
+      text: string
     }
     gallery: ImageProps[]
   }
@@ -27,6 +29,29 @@ export default function PlacesTemplate({ place }: PlacesTemplateProps) {
   if (router.isFallback) return null
   return (
     <>
+    <NextSeo
+        title={`${place.name} - My Trips`}
+        description={
+          place.description?.text ||
+          'A simple project to show in a map the places that I went and show more informations and photos when clicked.'
+        }
+        canonical="https://mytrips.com"
+        openGraph={{
+          url: 'https://mytrips.com',
+          title: `${place.name} - My Trips`,
+          description:
+            place.description?.text ||
+            'A simple project to show in a map the places that I went and show more informations and photos when clicked.',
+          images: [
+            {
+              url: place.gallery[0].url,
+              width: place.gallery[0].width,
+              height: place.gallery[0].height,
+              alt: `${place.name}`
+            }
+          ]
+        }}
+      />
       <LinkWrapper href="/">
         <CloseOutline size={32} aria-label="Go back to map" />
       </LinkWrapper>
