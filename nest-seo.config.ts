23f107e7321a10020f1e9f1e512e28180c5ff1df
Gabriellopes232 <gabriel.lopes.nascimento.dev@gmail.com>
2022-08-07 12:37:00 -0300

    chore: adding config seo

---
+++ nest-seo.config.ts
@@ -0,0 +1,13 @@
+export default {
+    openGraph: {
+      type: 'website',
+      locale: 'en_US',
+      url: 'https://my-trips.gabriellopes.com.br',
+      site_name: 'My Trips'
+    },
+    twitter: {
+      handle: '@gabriellopesxz',
+      site: '@site',
+      cardType: 'summary_large_image'
+    }
+  }
